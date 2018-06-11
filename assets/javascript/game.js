798af52415551377b1ff3e1bd9899e87c89f4b4a
Diana <dianajulaton@gmail.com>
2018-06-10 23:44:26 -0700

    edits and background image

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//when page is loaded, display a random number the user must match
+
+var randomNumber = 
+
+//random numbers is between 99-120
+
+//create 4 crystal images and attach values between 1-12
+
+//create an on click event for the crystal image
+
+//if user click score is over, they lose and reset game
+
+//if user's score matches random number, user wins
