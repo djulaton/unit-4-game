118bc95a8f5cb730ed5d1c7d241c931922239e24
Diana <dianajulaton@gmail.com>
2018-06-14 01:23:25 -0700

    edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,188 +1,154 @@
-//declare variable for wins, losses, runningTotal
+//make four crystal images with random values attached to 
+//each one as a data attribute or in the id
+
+//on click - add the value from the clicked element id or data attr
+//to the score
+
+//if they click and the score is over, they lose
+//if they win - show a success message
+
+//get rand number between 19 anmd 120
+//crystal number between 1 and 12
+
+
+
+//declare global variables
 
 var wins = 0;
-var loss = 0;
-var runningTotal = 0;
-var randomNum = 0;
+var losses = 0;
+var userTotal = 0;
+var randomNumber = 0;
 
-//create array that contains the crystals
+//create an array that holds the crystal points
 
-var crystalPoints = [];
+var crystalPoints = ['', '', '', ''];
 
-//create function for game reset
+//generate a random number between 19-120 that loads on the screen for the user to match
+//a new number should generate everytime we win or lose
 
-//generate random number between 19 - 120 and display on randomNumber div
+var resetAndStart = function() {
 
-/*function rand() {var randomNum = [Math.floor(Math.random() * 101 + 19)];
-$('#randomNumber').text(randomNum);
-}*/
-//generate 1 random number between 1-12 and add to array. No duplicates
+    $(".crystals").empty();
 
-    var randomNum = [Math.floor(Math.random() * 101 + 19)];
-    $('#randomNumber').text(randomNum);
+    var images = [
+                'blue-gem.jpg',
+                'https://www.rd.com/wp-content/uploads/2017/08/02_crystal_Reasons-Why-You-Should-Give-Crystals-a-Try_364248173_Abra-Cadabraaa-380x254.jpg',
+                'https://www.rd.com/wp-content/uploads/2017/08/02_crystal_Reasons-Why-You-Should-Give-Crystals-a-Try_364248173_Abra-Cadabraaa-380x254.jpg',
+                'https://www.rd.com/wp-content/uploads/2017/08/02_crystal_Reasons-Why-You-Should-Give-Crystals-a-Try_364248173_Abra-Cadabraaa-380x254.jpg']
 
+    randomNumber = Math.floor(Math.random() * 101 + 19);
+    $("#number-to-guess").text(randomNumber);
+    // console.log(randomNumber);
 
-    for (i=0; crystalPoints.length < 4; i++) {
-    var arNum = Math.floor(Math.random() * 12) + 1;
-    if (crystalPoints.indexOf(arNum) < 0) {
-    crystalPoints.push(arNum);
-    } 
-       console.log(crystalPoints);
-       console.log(runningTotal);
-       console.log(randomNum);
+    //create random values for 4 crystal images between 1-12
+
+    for (var i = 0; i < 4; i++) {
+
+        var random = Math.floor(Math.random() * 11) + 1;
+        console.log(random);
+
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal',
+            "data-random": random
+
+        });
+
+           crystal.css({
+            "background-image":"url('" + images[i] + "')",
+            "background-size":"cover"
+           })
+
+        $(".crystals").append(crystal);
+    }
+
 }
 
-       
+resetAndStart();
 
-function reset() {
+//on click -create a function to add the value of the crystal to the score
+//resets game
+$(document).on('click', ".crystal", function () {
 
-   
-   crystalPoints.length = 0;
-   runningTotal = 0;
-   randomNum = 0;
+        var num = parseInt($(this).attr('data-random')); 
+        userTotal += num;
 
-   $('#currentTotal').text(runningTotal);
-    
-    crystalPoints.length = 0; 
-    
-    runningTotal = 0;
-    
-    $('#currentTotal').text(runningTotal);
+        $("#score").html(userTotal)
 
-    randomNum = Math.floor(Math.random() * 101 + 19);
-    $('#randomNumber').text(randomNum);
+        console.log(userTotal);
 
-    for (i=0; crystalPoints.length < 4; i++) {
-        arNum = Math.floor(Math.random() * 12) + 1;
-        if (crystalPoints.indexOf(arNum) < 0) {
-        crystalPoints.push(arNum);
-        
-       } 
-       console.log(crystalPoints);
-       console.log(runningTotal);
-       console.log(randomNum);
-          
-    }
- } 
+        if (userTotal > randomNumber) {
+            console.log("You lost!!");
+            losses++;
+            alert("Sorry, try again!");
 
- 
-    
- /*if (randomNum == runningTotal) {
-    //winner();
-    winner();
-} else if (runningTotal > randomNum) {
-    //loser()
-    loser();
-}*/
+        $("#loss").html(losses);
 
-//win and lose functions
+        userTotal = 0;
 
-function winner() {
-    wins++;
-        console.log(wins);
-        $('#wins').text('Wins:   ' + wins);
-        alert("You win!");
-        reset();
+        resetAndStart();
+        }
+        else if (userTotal === randomNumber) {
+            console.log("You win!!");
+            wins++;
+            alert("Winner, winner, chicken dinner!");
+
+        $("#win").html(wins);
+
+        userTotal = 0;
+
+        resetAndStart();
+
+        }
+});
+
+
+//if the score is over, they lose and alert message pops up and resets game, increments a loss
+//if they match the randomNumber, they win and alert pops up and resets game and increments a win
+
+/*
+score.textContent = '0';
+
+var targetNums = [];
+
+for(var i=19; i<=120; i++){
+    targetNums.push(i);
 }
 
+//TODO: 
+//put target num on the screen
 
+var crystalNums = [];
 
-function loser() {
-        loss++;
-        $('#losses').text('Losses: ' + loss);
-        alert("Sorry you lose!")
-        reset();
+for(var i=1; i<=12; i++){
+    crystalNums.push(i);
 }
 
-/*if (randomNum == runningTotal) {
-    //winner();
-    winner();
-} else if (runningTotal > randomNum) {
-    //loser()
-    loser();
-}*/
+var targetNum = targetNums[Math.floor(Math.random()*targetNums.length)];
 
+for(var i=0; i<4; i++){
+    //make crystal elems 
+    var crys = document.createElement('img');
+    crys.className = 'crys';
+    crys.src = "http://www.seirtec.org/wp-content/uploads/ktz/chandelier-wooden-original-wooden-orb-chandelier-metal-orb-detail-and-crystal-img-365tpo8zqm3wdy29u7nsp6.jpg";
+    crys.dataset.hiddenValue = crystalNums[Math.floor(Math.random()*crystalNums.length)];
+    //get value to attach to crystal elems
 
+    document.body.appendChild(crys);
+}
 
-/*listen for click event and assign value to crystal
-clicked and add to total*/
+document.addEventListener('click', function(e){
+    if(e.target.className == 'crys'){
+        //get value of current score
+        var curScore = Number(document.getElementById('score').innerHTML);
+        var crysVal = Number(e.target.dataset.hiddenValue);
+        var newScore = curScore + crysVal;
 
+        document.getElementById('score').innerHTML = newScore;
+        //add the data attribute value to the score
 
-$('#crystal1').on('click', function(){
-    runningTotal = parseInt(runningTotal);
-    crystalPoints[0] = parseInt(crystalPoints[0]);
-    runningTotal = runningTotal + (crystalPoints[0]);
-    $('#currentTotal').text(runningTotal);
-
-    console.log(runningTotal);
-    console.log(randomNum);
-   
-
-    if (randomNum == runningTotal) {
-        //winner();
-        winner();
-
-    } else if (runningTotal > randomNum) {
-        //loser()
-        loser();
+        //
     }
-  
 })
 
-$('#crystal2').on('click', function(){
-    runningTotal = parseInt(runningTotal);
-    crystalPoints[1] = parseInt(crystalPoints[1]);
-    runningTotal = runningTotal + (crystalPoints[1]);
-    $('#currentTotal').text(runningTotal);
-
-    console.log(runningTotal);
-    console.log(randomNum);
-
-    if (randomNum == runningTotal) {
-        //winner();
-        winner();
-    } else if (runningTotal > randomNum) {
-        //loser()
-       loser();
-    }
-   
-})
-
-$('#crystal3').on('click', function(){
-    runningTotal = parseInt(runningTotal);
-    crystalPoints[2] = parseInt(crystalPoints[2]);
-    runningTotal = runningTotal + (crystalPoints[2]);
-    $('#currentTotal').text(runningTotal);
-
-    console.log(runningTotal);
-    console.log(randomNum);
-
-    if (randomNum == runningTotal) {
-       //winner();
-      winner();
-
-    } else if (runningTotal > randomNum) {
-        //loser()
-        loser();
-    }
- 
-})
-
-$('#crystal4').on('click', function(){
-    runningTotal = parseInt(runningTotal);
-    crystalPoints[3] = parseInt(crystalPoints[3]);
-    runningTotal = runningTotal + (crystalPoints[3]);
-    $('#currentTotal').text(runningTotal);
-
-    console.log(runningTotal);
-    console.log(randomNum);
-
-    if (randomNum == runningTotal) {
-        //winner();
-        winner();
-    } else if (runningTotal > randomNum) {
-        //loser()
-        loser();
-    }
-       
-})
+*/
